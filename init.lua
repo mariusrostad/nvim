d2b39f79b7b3e9414e2f46655b2c694dbddfd03b
Marius Rostad <marostad98@gmail.com>
2024-10-06 17:15:00 +0200

    feat: Added more language servers

--- init.lua
+++ init.lua
@@ -570,17 +570,28 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        ansiblels = {},
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        html = {},
+        tsserver = {},
+        cssls = {},
+        jsonls = {},
+        elmls = {},
+        jsonnet_ls = {},
+        kotlin_language_server = {},
+        ocamllsp = {},
+        sqlls = {},
+        terraformls = {},
+        tflint = {},
         --
 
         lua_ls = {
